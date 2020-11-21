dc0be696a5eaa72f552c7dbae8d22018acae046c
Diego Ortiz <ortizdiego@comcast.net>
2020-11-21 13:49:56 -0600

    Added to the schema.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,44 @@
 CREATE TABLE cart (
-    id    int          PRIMARY KEY AUTO_INCREMENT,
-    item  varchar(255) DEFAULT NULL,
-    qty   int          DEFAULT 1,
-    price float        DEFAULT 0
+    id     int          PRIMARY KEY AUTO_INCREMENT,
+    partID int          NOT NULL,
+    item   varchar(255) DEFAULT NULL,
+    qty    int          DEFAULT 1,
+    price  float        DEFAULT 0,
+
+    FOREIGN KEY (partID) REFERENCES parts(id)
 );
 
 CREATE TABLE taxes (
     id            int    PRIMARY KEY AUTO_INCREMENT,
     weightBracket int    NOT NULL,
     taxRate       float  NOT NULL
+);
+
+CREATE TABLE orders (
+    id            int    PRIMARY KEY AUTO_INCREMENT,
+    partID        int    NOT NULL,
+    qty           int    DEFAULT 1,
+    email         VARCHAR(255),
+    //if status status = 0, completed?
+    //          status = 1, processing?
+    //          status = 3, bad?
+    status        int DEFAULT 1,
+
+    FOREIGN KEY (partID) REFERENCES parts(id)
+);
+
+CREATE TABLE inventory (
+    id            int    PRIMARY KEY AUTO_INCREMENT,
+    partID        int    NOT NULL,
+    qty           int    DEFAULT 0,
+
+    FOREIGN KEY (partID) REFERENCES parts(id)
+);
+
+CREATE TABLE parts(
+    id            int    PRIMARY KEY AUTO_INCREMENT,
+    desc          VARCHAR(50)    NOT NULL,
+    price         decimal(8,2)   NOT NULL,
+    weight        decimal(4,2)   NOT NULL,
+    pictureURL    VARCHAR(50)    NOT NULL
 );
