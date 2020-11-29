3a6fd689b243e2639dbc3a7b90c0ffaeb44c89aa
Diego Ortiz <ortizdiego@comcast.net>
2020-11-29 15:49:12 -0600

    Had to add one partQty

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,12 +7,13 @@ CREATE TABLE parts (
 );
 
 CREATE TABLE cart (
-    id     int          PRIMARY KEY AUTO_INCREMENT,
-    partID int          NOT NULL,
-    item   varchar(255) DEFAULT NULL,
-    qty    int          DEFAULT 1,
-    price  decimal(8,2) DEFAULT 0,
-    weight decimal(4,2) DEFAULT 0,
+    id      int          PRIMARY KEY AUTO_INCREMENT,
+    partID  int          NOT NULL,
+    item    varchar(255) DEFAULT NULL,
+    qty     int          DEFAULT 1,
+    price   decimal(8,2) DEFAULT 0,
+    weight  decimal(4,2) DEFAULT 0,
+    partQty int          NOT NULL,
 
     FOREIGN KEY (partID) REFERENCES parts(id)
 );
