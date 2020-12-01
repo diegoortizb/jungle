0cccbdef27f88194cc24b58afcd2028e58163aa7
FORGEJackson <josh.forge.jackson@gmail.com>
2020-11-30 22:38:08 -0600

    HTML is good, but I'm having trouble figuring out how to actually update the database in SpringBoot

--- src/main/java/com/java/jungle/controllers/ReceivingDeskController.java
+++ src/main/java/com/java/jungle/controllers/ReceivingDeskController.java
@@ -1,22 +1,36 @@
 package com.java.jungle.controllers;
 
+import com.java.jungle.repository.Parts.PartsRepository;
 import com.java.jungle.service.Db2Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/rd")
 public class ReceivingDeskController {
 
     @Autowired
-    private Db2Service parts;
+    private PartsRepository partsRepo;
+//    private Db2Service parts;
 
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("parts", parts.findAll());
+        model.addAttribute("Inventory", partsRepo.getCustomerView());
+//        model.addAttribute("Inventory", parts.findAll());
         return "receivingDesk";
+    }
+
+    @RequestMapping(value="/rd", method= RequestMethod.POST)
+    public String addItemToCart(@RequestParam(value="partID") Integer partID,
+                                @RequestParam(value="invUpdate") int updateQuantity) {
+
+        //TODO: Update partID's quantity
+
+        return "redirect:/rd";
     }
 }
